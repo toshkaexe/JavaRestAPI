fdbe6990309f1418ba5f42fb542eda6ae975b85a
AntonZ <anton.zeltser@retarus.de>
2021-02-21 19:37:47 +0100

    added loadDataFromJsonArray to the UserService

--- restfull/src/main/java/server/restfull/controller/UserController.java
+++ restfull/src/main/java/server/restfull/controller/UserController.java
@@ -1,76 +1,79 @@
 
-
 package server.restfull.controller;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.TimeZone;
+
 import server.restfull.model.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.log.LogFormatUtils;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import server.restfull.model.User;
 import server.restfull.repo.UserRepo;
+import org.apache.commons.logging.Log;
+
+import org.apache.juli.logging.LogFactory;
+import org.json.JSONObject;
+import org.json.JSONObject;
+import org.apache.commons.logging.Log;
 
 @CrossOrigin(origins = "http://localhost:3000")
+
 @RestController
 public class UserController {
 
-    @Autowired
+	@Autowired
 	private UserService userService;
 
-    @RequestMapping("users")
-    public List<User> getAllUsers()
-	{
+	@RequestMapping("/")
+	@ResponseBody
+	public String welcome() {
+
+		return "login";
+	}
+
+	@RequestMapping(value = "users", method = RequestMethod.GET)
+	public List<User> getAllUsers() {
 		return userService.getAllUsers();
 	}
-    @RequestMapping("uuu")
-    public void ttt() {
-    	
-    	System.out.println("AAAAAAAAAAAAAAA");
-    }
-    
-    
-    @RequestMapping(value = "users/{id}", method = RequestMethod.GET)
-    public User getUserById(@PathVariable String id) {
-        System.out.println("ID is " + id);
-        
-        if ( userService.getUserById(id) == (null))
-        {
-        	System.out.println(String.format("We do not have user with id=%s",id));
-        	return null;
-        }
-        else  return userService.getUserById(id);
-      //  return "Dynamic URI parameter fetched";
-    }
-    
 
+	@RequestMapping(value = "users/{id}", method = RequestMethod.GET, produces = "application/json")
+	public User getUserById(@PathVariable String id) {
+		System.out.println("ID is " + id);
 
-	@RequestMapping(method=RequestMethod.POST, value ="users")
-	public void addUser(@RequestBody User user)
-	{
-		userService.addUser(user);
+		if (userService.getUserById(id) == (null)) {
+			System.out.println(String.format("Sorry, but we do not have user with id=%s", id));
+			return null;
+		} else
+			return userService.getUserById(id);
 	}
-	
-	@RequestMapping(method=RequestMethod.PUT, value ="users/{id}")
-	public void updateUser(@RequestBody User user, @PathVariable int id)
-	{
-		userService.updateUserList(id,user);
+
+	@RequestMapping(method = RequestMethod.POST, value = "users")
+	public void addUser(@RequestBody User user) {
+		userService.addUser(new User("11", "Mana", "Papa", "Pap@gail.com"));
 	}
-	
-	@RequestMapping(method=RequestMethod.DELETE, value ="users/{id}")
-	public void deleteUser(@RequestBody User user, @PathVariable int id)
-	{
+
+	@RequestMapping(method = RequestMethod.PUT, value = "users/{id}", produces = "application/json")
+	public void updateUser(@RequestBody User user, @PathVariable int id) {
+		userService.updateUserList(id, user);
+	}
+
+	@RequestMapping(method = RequestMethod.DELETE, value = "users/{id}")
+	public void deleteUser(@RequestBody User user, @PathVariable int id) {
 		userService.deleteUserList(id);
-	 	System.out.println("AAAAAAAAAAAAAAABBBBBBBBBBBBBB");
+		System.out.println("AAAAAAAAAAAAAAABBBBBBBBBBBBBB");
 	}
-	
-		
-	
-}
 
+}
--- restfull/src/main/java/server/restfull/model/UserService.java
+++ restfull/src/main/java/server/restfull/model/UserService.java
@@ -6,103 +6,125 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import aj.org.objectweb.asm.TypeReference;
+
+import org.apache.tomcat.util.json.ParseException;
+import org.json.simple.JSONArray;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+
 @Service
 public class UserService {
 
-	
-	private List <User>  users = new ArrayList<>(Arrays.asList(
-			new User("1", "Anton", "Zzz", "anton.zzz@gmail.com"),
-			new User("2", "Boni", "Noro","boni.noro@com.com"),
-			new User("3", "Tino", "Bach", "bach@p2.com")
-			));
-	
-	
-	public List<User> getAllUsers() {
-		
+	private List<User> users = new ArrayList<>();
+
+	public UserService() throws org.json.simple.parser.ParseException {
+		List<User> users = new ArrayList<>();
+		users = loadDataFromJsonArray();
+
+		this.users = users;
+
+	}
+
+	List<User> loadDataFromJsonArray() throws org.json.simple.parser.ParseException {
+		List<User> users = new ArrayList<>();
+		// JSON parser object to parse read file
+		JSONParser jsonParser = new JSONParser();
+		try (FileReader reader = new FileReader("../restfull/src/main/resources/userlist.json")) {
+			// Read JSON file
+			Object obj = jsonParser.parse(reader);
+			JSONObject jsonObject = (JSONObject) obj;
+			// extracting data array from json string
+			JSONArray usersArray = (JSONArray) jsonObject.get("users");
+			for (int i = 0; i < usersArray.size(); i++) {
+				JSONObject userItem = (JSONObject) usersArray.get(i);
+
+				System.out.println(String.format("item = %d, %s, - %s - %s, - %s", i, userItem.get("id"), userItem.get("name"),
+						userItem.get("lastname"), userItem.get("email")));
+
+				users.add(new User(userItem.get("id").toString(), userItem.get("name").toString(),
+						userItem.get("lastname").toString(), userItem.get("email").toString()));
+
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return users;
 	}
-	
-	
-	public User getUserById(String id)
-	{
-		
-		for(int i=0; i<users.size(); i++) {
-		User u = users.get(i);
-	
-			if ((u.getId()).equals(id)) {
-				
-			return users.get(i);
-			
-			}
-			
-			}
-		return null;
-		
-	
-	}
-	
-	public void addUser(User user) {
-				users.add(user);	
-				
-	}
-	
-	public void updateUserList(int id, User user)
-	{
-		for (int i = 0; i<users.size(); i++)
-		 {
-			User u = users.get(i);
-				if ((u.getId()).equals(id))
-						
-						{        
-			         users.set(id, user);	
-			         return;
-			         
-					
-		}
-			
-			}
-	}
-	
 
-	
-	
-	public User getUserwithId(int id)
-	{
-		for (int i = 0; i<users.size(); i++)
-		 {
+	public List<User> getAllUsers() {
+
+		return users;
+	}
+
+	public User getUserById(String id) {
+
+		for (int i = 0; i < users.size(); i++) {
 			User u = users.get(i);
-				if (u.getId().equals(id))
-						
-			         	
-			         return u;
-					
-		
-			
+
+			if ((u.getId()).equals(id)) {
+
+				return users.get(i);
+
 			}
+
+		}
+		return null;
+
+	}
+
+	public void addUser(User user) {
+		users.add(user);
+
+	}
+
+	public void updateUserList(int id, User user) {
+		for (int i = 0; i < users.size(); i++) {
+			User u = users.get(i);
+			if ((u.getId()).equals(id))
+
+			{
+				users.set(id, user);
+				return;
+
+			}
+
+		}
+	}
+
+	public User getUserwithId(int id) {
+		for (int i = 0; i < users.size(); i++) {
+			User u = users.get(i);
+			if (u.getId().equals(id))
+
+				return u;
+
+		}
 		return null;
 	}
 
 	public void deleteUserList(int id) {
 		// TODO Auto-generated method stub
-		for (int i = 0; i<users.size(); i++)
-		 {
+		for (int i = 0; i < users.size(); i++) {
 			User u = users.get(i);
-				if (u.getId().equals(id))
-						{
-			         users.remove(id);	
-			         return;
-					
-		}
-			
+			if (u.getId().equals(id)) {
+				users.remove(id);
+				return;
+
 			}
-		
+
+		}
+
 	}
 
-
-
-
-	
-	
 }
-
-	
