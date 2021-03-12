fef3c3d723a0933a4098a5e41f6bee2c23cef308
AntonZ <anton.zeltser@retarus.de>
2021-03-12 16:42:17 +0100

    cleaning and added unit-tests

--- frontend/src/service/Utils.test.js
+++ frontend/src/service/Utils.test.js
@@ -1,22 +1,17 @@
 import Utils from "./Utils";
 
 describe("Utils Suite", () => {
-
   it("UserValue Empty", () => {
     expect(false).toEqual(Utils.isValueNotEmpty(""));
   });
-
 
   it("UserValue is not Empty", () => {
     expect(true).toEqual(Utils.isValueNotEmpty("Max Mustermann"));
   });
 
-
-
   it("Email is ok", () => {
     expect(true).toEqual(Utils.isNotEmptyEmail("Max.Mustermann@gmail.com"));
   });
-
 
   it("Email is not ok case 1", () => {
     expect(false).toEqual(Utils.isNotEmptyEmail(""));
@@ -32,5 +27,4 @@ describe("Utils Suite", () => {
   it("Email is not ok case 4", () => {
     expect(false).toEqual(Utils.isNotEmptyEmail("Max@m@.de"));
   });
-
 });
